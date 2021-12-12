dbc4aed5ae6240fa266844f6cfcff3e947d960d4
iakov <iakovan2329@gmail.com>
2021-12-12 21:03:26 +0500

    Nineth Commit

--- scripts/script.js
+++ scripts/script.js
@@ -55,16 +55,16 @@ var TxtRotate = function(el, toRotate, period) {
   };
 
 
-function onEntry(entry) {
-  entry.forEach(change => {
-    if (change.isIntersecting) {
-      change.target.classList.add('cards-show');
-    }
-  });
-}
-let options = { threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.cards');
-for (let elm of elements) {
-  observer.observe(elm);
-}
+// function onEntry(entry) {
+//   entry.forEach(change => {
+//     if (change.isIntersecting) {
+//       change.target.classList.add('cards-show');
+//     }
+//   });
+// }
+// let options = { threshold: [0.5] };
+// let observer = new IntersectionObserver(onEntry, options);
+// let elements = document.querySelectorAll('.cards');
+// for (let elm of elements) {
+//   observer.observe(elm);
+// }
