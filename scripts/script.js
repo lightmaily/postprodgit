ef61d398360ae8912f8b984820bda36fb964a417
iakov <iakovan2329@gmail.com>
2021-12-12 14:07:24 +0500

    Eighth Commit

--- scripts/script.js
+++ scripts/script.js
@@ -69,14 +69,12 @@ for (let elm of elements) {
   observer.observe(elm);
 }
 
-const smoothLinks = document.querySelectorAll('a[href="#cards"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-};
+$('a[href^="#"').on('click', function() {
+
+  let href = $(this).attr('href');
+
+  $('html, body').animate({
+      scrollTop: $(href).offset().top
+  });
+  return false;
+});
